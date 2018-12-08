1db5613044d27d06ac22af3a372863f5077c0f66
Leo Sklyut <lgsklyut@gmail.com>
2018-12-08 13:49:30 -0500

    complete the test scenario
    I added:
    
    1) setup code which creates a student with 5 books and 3 gadgets
    2) code which removes the books, adds them back, removes the gadgets, adds them back, and flushes.
    
    At the end of this, there are twice as many books as we expect. The difference between this and the issue we're seeing at work is - at work, adding $entityManager->flush() between the book and gadget setup resolves the issue. With this code, I have no such luck.

--- README.md
+++ README.md
@@ -1,15 +1,3 @@
 1) composer install
 2) ./vendor/bin/doctrine orm:schema-tool:create
 3) php ./index.php
-
-This code is not reproducing the issue I'm looking for yet. The result I'm trying to achieve is the creation of
-duplicate Books - instead of creating 5, there should be 10. Currently, the code creates:
-
-1 Student
-5 Books
-3 Gadgets
-
-Expected Result to reproduce the issue we're seeing at work:
-1 Student
-10 Books
-3 Gadgets
--- index.php
+++ index.php
@@ -1,35 +1,104 @@
 <?php
 
+use Doctrine\ORM\EntityManager;
 use LeoDoctrineBug\Entity\Book;
 use LeoDoctrineBug\Entity\Gadget;
 use LeoDoctrineBug\Entity\Student;
 
+const STARTING_NUMBER_OF_GADGETS = 3;
 require 'bootstrap.php';
 
-$student = new Student();
-$student->setName('Leo');
+const STARTING_NUMBER_OF_BOOKS = 5;
 
-$entityManager->persist($student);
-$entityManager->flush();
+/**
+ * @param EntityManager $entityManager
+ */
+function testScenarioCreatesDuplicates(EntityManager $entityManager)
+{
+    $student = setUpStudent($entityManager);
 
-foreach (range(1, 5) as $i) {
-    $book = new Book();
-    $book->setName("Book {$i}");
-    $book->setStudent($student);
-    $student->addBook($book);
+    // At this point we have a student with 5 books and 3 gadgets.
+
+    foreach ($student->getBooks() as $book) {
+        $entityManager->remove($book);
+        $entityManager->flush($book);
+    }
+
+    setUpBooksForStudent($entityManager, $student);
+
+    foreach ($student->getGadgets() as $gadget) {
+        $entityManager->remove($gadget);
+        $entityManager->flush($gadget);
+    }
+
+    setUpGadgetsForStudent($entityManager, $student);
+
+    $entityManager->flush();
+
+    // At this point we have a student with 10 books and 6 gadgets.
+
+    $wereBooksDuplicated = count($student->getBooks()) == 2 * STARTING_NUMBER_OF_BOOKS;
+
+    print "Books were " . ($wereBooksDuplicated ? '' : 'not ') . 'duplicated.' . PHP_EOL;
+
+    $wereGadgetsDuplicated = count($student->getGadgets()) == 2 * STARTING_NUMBER_OF_GADGETS;
+
+    print "Gadgets were " . ($wereGadgetsDuplicated ? '' : 'not ') . 'duplicated.' . PHP_EOL;
 }
 
-$gadgets = ['laptop', 'phone', 'smartwatch'];
+/**
+ * @param EntityManager $entityManager
+ * @return Student
+ */
+function setUpStudent(EntityManager $entityManager)
+{
+    $student = new Student();
+    $student->setName('Leo');
 
-foreach ($gadgets as $gadgetName) {
-    $gadget = new Gadget();
-    $gadget->setName($gadgetName);
+    $entityManager->persist($student);
 
-    $gadget->setStudent($student);
-    $student->addGadget($gadget);
+    setUpBooksForStudent($entityManager, $student);
+    setUpGadgetsForStudent($entityManager, $student);
 
-    $entityManager->persist($gadget);
-    $entityManager->flush($gadget);
+    $entityManager->flush();
+    return $student;
 }
 
-$entityManager->flush();
+/**
+ * @param EntityManager $entityManager
+ * @param Student $student
+ */
+function setUpBooksForStudent(EntityManager $entityManager, Student $student)
+{
+    foreach (range(1, STARTING_NUMBER_OF_BOOKS) as $i) {
+        $book = new Book();
+        $book->setName("Book {$i}");
+        $book->setStudent($student);
+        $student->addBook($book);
+
+        $entityManager->persist($book);
+        $entityManager->flush($book);
+    }
+}
+
+/**
+ * @param EntityManager $entityManager
+ * @param Student $student
+ */
+function setUpGadgetsForStudent(EntityManager $entityManager, Student $student)
+{
+    $gadgets = ['laptop', 'phone', 'smartwatch'];
+
+    foreach ($gadgets as $gadgetName) {
+        $gadget = new Gadget();
+        $gadget->setName($gadgetName);
+
+        $gadget->setStudent($student);
+        $student->addGadget($gadget);
+
+        $entityManager->persist($gadget);
+        $entityManager->flush($gadget);
+    }
+}
+
+testScenarioCreatesDuplicates($entityManager);
